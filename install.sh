31af408218335d391f4e6713f6c8288a6ff2c88a
Joel Serino <joel@thinq4yourself.com>
2020-11-02 13:36:50 -0800

    updatonig scripts

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Pre-release Q3 2020 - Canary - 3.8.0-canary.rc.0
 readonly VERSION="3.8.0-canary.rc.0"
-readonly GITLAB_TOKEN=""
+readonly GITLAB_TOKEN=$GITLAB_API_TOKEN
 readonly CHATCTL_DOWNLOAD_URL="https://gitlab.com/mc2labs/universe/chat/appkit/InstallUpdater/-/raw/master/chatctl"
 readonly CHATCTL_DIRECTORY="/usr/local/bin"
 
@@ -11,12 +11,12 @@ if [ ${EUID} -ne 0 ]; then
     exit 1
 fi
 if ! [[ -t 0 ]]; then
-    echo "This script is interactive, please run: bash -c \"\$(curl https://gitlab.com/mc2labs/universe/chat/appkit/InstallUpdater/-/raw/master/chatctl --gitlab-token=YOUR_TOKEN)\"" >&2
+    echo "This script is interactive, please run: bash -c \"\$(curl https://gitlab.com/mc2labs/universe/chat/appkit/InstallUpdater/-/raw/master/chatctl?private_token=YOUR_TOKEN)\"" >&2
     exit 1
 fi
-if [ ! -f "$CHATCTL_DIRECTORY/chatctl" ]; then
+if [ ! -f "$CHATCTL_DIRECTORY" ]; then
     if [ ${#args[@]} -ne 2 ]; then
-        curl -L $CHATCTL_DOWNLOAD_URL?private_token=${args[1]} -o /tmp/chatctl
+        curl -L $CHATCTL_DOWNLOAD_URL?private_token=$GITLAB_TOKEN -o /tmp/chatctl
     fi
     if  [ $? != 0 ]; then
         echo "Error downloading chatctl."
