d6d872008b2be9393553529808c900d0985a4b5a
Joel Serino <joel@thinq4yourself.com>
2020-11-02 11:23:10 -0800

    Prepare chatctl for ubuntu deployment management

--- README.md
+++ README.md
@@ -3,6 +3,5 @@
 To use the released version of this you can run:
 
 ```
-bash -c "$(curl https://install.rocket.chat)"
+bash -c "$(curl https://install.playmc2.org/universe/chat --gitlab-token=YOUR_TOKEN)"
 ```
-
--- install.sh
+++ install.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
-readonly ROCKETCHATCTL_DOWNLOAD_URL="https://install.rocket.chat/rocketchatctl"
-readonly ROCKETCHATCTL_DIRECTORY="/usr/local/bin"
+# Pre-release Q3 2020 - Canary - 3.8.0-canary.rc.0
+readonly VERSION="3.8.0-canary.rc.0"
+readonly GITLAB_TOKEN=""
+readonly CHATCTL_DOWNLOAD_URL="https://gitlab.com/mc2labs/universe/chat/appkit/InstallUpdater/-/raw/master/chatctl"
+readonly CHATCTL_DIRECTORY="/usr/local/bin"
 
 if [ ${EUID} -ne 0 ]; then
     echo "This script must be run as root. Cancelling" >&2
     exit 1
 fi
 if ! [[ -t 0 ]]; then
-    echo "This script is interactive, please run: bash -c \"\$(curl https://install.rocket.chat)\"" >&2
+    echo "This script is interactive, please run: bash -c \"\$(curl https://gitlab.com/mc2labs/universe/chat/appkit/InstallUpdater/-/raw/master/chatctl --gitlab-token=YOUR_TOKEN)\"" >&2
     exit 1
 fi
-if [ ! -f "$ROCKETCHATCTL_DIRECTORY/rocketchatctl" ]; then
-    curl -L $ROCKETCHATCTL_DOWNLOAD_URL -o /tmp/rocketchatctl
+if [ ! -f "$CHATCTL_DIRECTORY/chatctl" ]; then
+    if [ ${#args[@]} -ne 2 ]; then
+        curl -L $CHATCTL_DOWNLOAD_URL?private_token=${args[1]} -o /tmp/chatctl
+    fi
     if  [ $? != 0 ]; then
-        echo "Error downloading rocketchatctl."
+        echo "Error downloading chatctl."
         exit 1
     else
-        mv /tmp/rocketchatctl $ROCKETCHATCTL_DIRECTORY/
-        chmod 755 $ROCKETCHATCTL_DIRECTORY/rocketchatctl
+        mv /tmp/chatctl $CHATCTL_DIRECTORY/
+        chmod 755 $CHATCTL_DIRECTORY/chatctl
     fi
-    $ROCKETCHATCTL_DIRECTORY/rocketchatctl install $@
+    $CHATCTL_DIRECTORY/chatctl install $@
 else
-    echo "You already have rocketchatctl installed, use rocketchatctl to manage your RocketChat installation."
-    echo "Run rocketchatctl help for more info."
+    echo "You already have chatctl installed, use chatctl to manage your UniverseChat installation."
+    echo "Run chatctl help for more info."
     exit 1
 fi
